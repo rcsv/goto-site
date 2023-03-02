980276bcc72aeeabbf0b070d579af3d7eda2ef14
rcsv <rcsv@users.noreply.github.com>
2023-03-02 21:43:34 +0900

    Create db_info.php

---
+++ app/config/db_info.php
@@ -0,0 +1,9 @@
+<?php
+/// goto-site service prototype in PHP
+/// I want to exclude information required for databae connections from the web root
+/// you can use them using require_once("../config/db_info.php");.
+
+$db_host = 'localhost' ;
+$db_name = 'shorteners' ;
+$db_user = 'root' ;
+$db_password = '';
